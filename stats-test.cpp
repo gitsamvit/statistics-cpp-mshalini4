f3711f3281015f124203c0b7200670c12b1554a4
mshalini4 <105044776+mshalini4@users.noreply.github.com>
2022-05-06 21:12:20 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -18,7 +18,9 @@ TEST_CASE("average is NaN for empty array") {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
-    //Design the REQUIRE statement here.
+     REQUIRE(Catch::isnan(computedStats.average));
+    REQUIRE(Catch::isnan(computedStats.max));
+    REQUIRE(Catch::isnan(computedStats.min));
     //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
 
