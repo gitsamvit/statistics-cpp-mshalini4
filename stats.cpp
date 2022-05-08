0321f0853f5bc924552418282060cd3f5fc94fea
mshalini4 <105044776+mshalini4@users.noreply.github.com>
2022-05-08 21:38:54 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,33 +1,33 @@
-#include<iostream>
-#include <vector>
-#include<math.h>
+#include "stats.h"
+#include<cmath>
 #include<bits/stdc++.h>
-#include<numeric>
-using namespace std;
 
-namespace Statistics
+template<typename T> Statistics::Stats<>::Stats(){}
+template<typename T> Statistics::Stats<T,T,T>::Stats(T Average,T Max,T Min):Average(Average),Max(Max),Min(Min){}
+template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+//Statistics::Stats::Stats(){}
+//Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
+//Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
+
 {
+    Stats<T> st;
+    if(data.size()==0)
+    {
+       st.Average=NAN;
+       st.Max=NAN;
+       st.Min=NAN;
+    }
     
-	template <typename T=double>	//default
-	class Stats
-   	 {
-	   public:
-		T Average;
-		T Max;
-		T Min;
-		//float Average;
-		//float Max;
-		//float Min;
-		Stats();//empty array
-		//Stats(float avg,float max,float min);
-		Stats(T average,T Max,T Min);
-	};
-	template<typename T=double>
-	Stats<T> ComputeStatistics(const std::vector<T>& );
-	//Stats ComputeStatistics(const std::vector<float>&);
+    st.Max=*max_element(data.begin(),data.end());
+    st.Min=*min_element(data.begin(),data.end());
+    T sum=accumulate(data.begin(), data.end() , 0);
+    st.Average=(sum/data.size());
+    return st;
+   // float maxi=*max_element(data.begin(),data.end());
+    //float mini=*min_element(data.begin(),data.end());
+    //float sum=accumulate(data.begin(), data.end() , 0);
+    //float avg=sum/data.size();
+    //return Statistics::Stats(avg,maxi,mini);
+
+    
 }
-//decalartion
-	
-	
-    
-	
