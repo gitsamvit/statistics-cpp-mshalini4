0ffbf947215db738f3fe6a7bddaa7270eff707d8
mshalini4 <105044776+mshalini4@users.noreply.github.com>
2022-05-08 17:25:16 +0530

    Update stats.cpp
    stats.cpp changes

--- stats.cpp
+++ stats.cpp
@@ -1,6 +1,22 @@
 #include "stats.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
-    int x=0;
+   Stats<T> st;
+    if(data.size()==0)
+    {
+       st.Average=NAN;
+       st.Max=NAN;
+       st.Min=NAN;
+    }
+    
+    st.Max=*max_element(data.begin(),data.end());
+    st.Min=*min_element(data.begin(),data.end());
+    T sum=accumulate(data.begin(), data.end() , 0);
+    st.Average=(sum/data.size());
+    return st;
+   // float maxi=*max_element(data.begin(),data.end());
+    //float mini=*min_element(data.begin(),data.end());
+    //float sum=accumulate(data.begin(), data.end() , 0);
+    //float avg=sum/data.size();
+    //return Statistics::Stats(avg,maxi,mini);
 }
